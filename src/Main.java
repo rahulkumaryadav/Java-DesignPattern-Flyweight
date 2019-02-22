59ea929527c295a34aceadeb8f5457d9a4f3c5ba
rahulkumaryadav <rahulkumar96369@gmail.com>
2019-02-22 11:42:54 +0530

    partially working condition is commited

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    private static final String color[]={"Red","Blue","pink","Brown"};
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    private static String getRandomColor(){
+        return color[(int)(Math.random()*color.length)];
+    }
+
+
+    /*private static int getRandom(){
+        return (int )(Math.random()))
+    }*/
+}
